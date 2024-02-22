596609030f393ab78d7ce99d90e762ae5a28c4fc
Reece Harris <reece.harris98@protonmail.com>
2024-02-22 23:59:47 +0000

    Update styles and templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./views/**/**.{html,ejs}', './public/js/*.js'],
+  content: ['./views/**/**.{html,handlebars}', './public/js/*.js'],
   theme: {
     extend: {
       colors: {
         clinically: "#524c56",
-        "chinese-black": "#100D12"
+        background: "#131313",
+        foreground: "#151515",
+        input: "#1c1c1c",
+        label: "#ccc",
+        faint: "#6b696c",
+        button: "#bdbdbd"
       }
     }
   },
